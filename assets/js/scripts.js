a7ea37bb7b9b60a7d549ad5e696090b71f18cbc4
eccegallery <hddowdy@ncsu.edu>
2022-01-28 13:49:57 -0600

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,95 +1,218 @@
-var cityHistory = [];
-var apiKey = "8d27d3e5298217240e160a88d8accd8c";
-var resultElement = document.getElementById("result");
-var weatherForecast = document.getElementById("weather-forecast");
-var cityForm = document.getElementById("city-form");
-var cityInput = document.getElementById("city-name");
-var searchBtn = document.getElementById("searchBtn");
-var historyContainer = document.getElementById("city-history");
-
-
-var nameEl =document.createElement("a")
-
-nameEl.setAttribute("class","city-name");
-nameEl.setAttribute("href", "./second-page.html");
-
-var iconEl =document.createElement("img");
-// assign an SRC attribute to hold the icon URL
-iconEl.setAttribute("src","");
-var tempEl= document.createElement("p");
-var humidityEl= document.createElement("p");
-var windEl= document.createElement("p");
-var descEl= document.createElement("p");
-// appending the dynamically created element
-resultElement.append(nameEl,iconEl,tempEl,humidityEl,windEl,descEl);
-
-function appendToHistory(city) {
-    // If there is no search term return the function
-    if (cityHistory.indexOf(city) !== -1) {
-    return;
-  }
-  cityHistory.push(city);
-
- localStorage.setItem('search-history',JSON.stringify(cityHistory));
-}
-
-
-var getCity = function(event){ 
-    event.preventDefault();
-    var cityName = cityInput.value.trim();
-    if(cityName){
-        getWeatherInfo(cityName);
-        //cityInput.value="";
-    }else{
-        throw("Error City not Found",{
-            buttons: {
-                cancel:true,
-                confirm:false,
-            },
-         })
+// set global variables
+var citiesListArr = [];
+var numOfCities = 9;
+var personalAPIKey = "appid=264dbcaa3899de05eeadc78d68ba06dc";
+var unit = "units=imperial";
+var dailyWeatherApiStarts =
+  "https://api.openweathermap.org/data/2.5/weather?q=";
+var dailyUVIndexApiStarts = "https://api.openweathermap.org/data/2.5/uvi?";
+var forecastWeatherApiStarts =
+  "https://api.openweathermap.org/data/2.5/onecall?";
+// select from html element
+var searchCityForm = $("#searchCityForm");
+var searchedCities = $("#searchedCityLi");
+//-------------------------- get weather info from OpenWeather starts here ------------------------------//
+var getCityWeather = function (searchCityName) {
+  // formate the OpenWeather api url
+  var apiUrl =
+    dailyWeatherApiStarts + searchCityName + "&" + personalAPIKey + "&" + unit;
+  // make a request to url
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      return response.json().then(function (response) {
+        $("#cityName").html(response.name);
+        // display date
+        var unixTime = response.dt;
+        var date = moment.unix(unixTime).format("MM/DD/YY");
+        $("#currentdate").html(date);
+        // display weather icon
+        var weatherIncoUrl =
+          "http://openweathermap.org/img/wn/" +
+          response.weather[0].icon +
+          "@2x.png";
+        $("#weatherIconToday").attr("src", weatherIncoUrl);
+        $("#tempToday").html(response.main.temp + " \u00B0F");
+        $("#humidityToday").html(response.main.humidity + " %");
+        $("#windSpeedToday").html(response.wind.speed + " MPH");
+        // return coordinate for getUVIndex to call
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        getUVIndex(lat, lon);
+        getForecast(lat, lon);
+      });
+    } else {
+      alert("Please provide a valid city name.");
     }
-}
-
-var getWeatherInfo = function (city) {
-    const apiUrl = 
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&units=imperial&appid=" +
-    apiKey;
+  });
+};
+var getUVIndex = function (lat, lon) {
+  // formate the OpenWeather api url
+  var apiUrl =
+    dailyUVIndexApiStarts +
+    personalAPIKey +
+    "&lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&" +
+    unit;
   fetch(apiUrl)
     .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          appendToHistory(city);
-          getWeather(data, city);
-        });
-      } else {
-        throw("Error City not found",{
-            buttons: {
-                cancel:true,
-                confirm:false,
-            },
-         })
-      }
+      return response.json();
     })
-    .catch(function (error) {
-        throw("error City not found",{
-          buttons: {
-              cancel:true,
-              confirm:false,
-          },
-       })
-      });
-  };
+    .then(function (response) {
+      // remove all class background
+      $("#UVIndexToday").removeClass();
+      $("#UVIndexToday").html(response.value);
+      if (response.value < 3) {
+        $("#UVIndexToday").addClass("p-1 rounded bg-success text-white");
+      } else if (response.value < 8) {
+        $("#UVIndexToday").addClass("p-1 rounded bg-warning text-white");
+      } else {
+        $("#UVIndexToday").addClass("p-1 rounded bg-danger text-white");
+      }
+    });
+};
+var getForecast = function (lat, lon) {
+  // formate the OpenWeather api url
+  var apiUrl =
+    forecastWeatherApiStarts +
+    "lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&exclude=current,minutely,hourly" +
+    "&" +
+    personalAPIKey +
+    "&" +
+    unit;
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      for (var i = 1; i < 6; i++) {
+        //display date
+        var unixTime = response.daily[i].dt;
+        var date = moment.unix(unixTime).format("MM/DD/YY");
+        $("#Date" + i).html(date);
+        // display weather icon
+        var weatherIncoUrl =
+          "http://openweathermap.org/img/wn/" +
+          response.daily[i].weather[0].icon +
+          "@2x.png";
+        $("#weatherIconDay" + i).attr("src", weatherIncoUrl);
+        // display temperature
+        var temp = response.daily[i].temp.day + " \u00B0F";
+        $("#tempDay" + i).html(temp);
+        // display humidity
+        var humidity = response.daily[i].humidity;
+        $("#humidityDay" + i).html(humidity + " %");
+      }
+    });
+};
+//-------------------------- get weather info from OpenWeather ends here ------------------------------//
+//-------------------------------------- create button starts  ----------------------------------------//
+var creatBtn = function (btnText) {
+  var btn = $("<button>")
+    .text(btnText)
+    .addClass("list-group-item list-group-item-action")
+    .attr("type", "submit");
+  return btn;
+};
+//-------------------------------------- create button ends  ------------------------------------------//
+//---------------------- load saved citeis names from localStorage starts here ------------------------//
+var loadSavedCity = function () {
+  citiesListArr = JSON.parse(localStorage.getItem("weatherInfo"));
+  if (citiesListArr == null) {
+    citiesListArr = [];
+  }
+  for (var i = 0; i < citiesListArr.length; i++) {
+    var cityNameBtn = creatBtn(citiesListArr[i]);
+    searchedCities.append(cityNameBtn);
+  }
+};
+//---------------------- load saved citeis names from localStorage ends here ------------------------//
+//----------------------- save searched city in to local storage starts here --------------------------//
+var saveCityName = function (searchCityName) {
+  var newcity = 0;
+  citiesListArr = JSON.parse(localStorage.getItem("weatherInfo"));
+  if (citiesListArr == null) {
+    citiesListArr = [];
+    citiesListArr.unshift(searchCityName);
+  } else {
+    for (var i = 0; i < citiesListArr.length; i++) {
+      if (searchCityName.toLowerCase() == citiesListArr[i].toLowerCase()) {
+        return newcity;
+      }
+    }
+    if (citiesListArr.length < numOfCities) {
+      // create object
+      citiesListArr.unshift(searchCityName);
+    } else {
+      // control the length of the array. do not allow to save more than 10 cities
+      citiesListArr.pop();
+      citiesListArr.unshift(searchCityName);
+    }
+  }
+  localStorage.setItem("weatherInfo", JSON.stringify(citiesListArr));
+  newcity = 1;
+  return newcity;
+};
+//------------------------ save searched city in to local storage ends here ---------------------------//
+//-------------------------- create button with searched city starts here -----------------------------//
+var createCityNameBtn = function (searchCityName) {
+  var saveCities = JSON.parse(localStorage.getItem("weatherInfo"));
+  // check the searchCityName parameter against all children of citiesListArr
+  if (saveCities.length == 1) {
+    var cityNameBtn = creatBtn(searchCityName);
+    searchedCities.prepend(cityNameBtn);
+  } else {
+    for (var i = 1; i < saveCities.length; i++) {
+      if (searchCityName.toLowerCase() == saveCities[i].toLowerCase()) {
+        return;
+      }
+    }
+    // check whether there are already have too many elements in this list of button
+    if (searchedCities[0].childElementCount < numOfCities) {
+      var cityNameBtn = creatBtn(searchCityName);
+    } else {
+      searchedCities[0].removeChild(searchedCities[0].lastChild);
+      var cityNameBtn = creatBtn(searchCityName);
+    }
+    searchedCities.prepend(cityNameBtn);
+    $(":button.list-group-item-action").on("click", function () {
+      BtnClickHandler(event);
+    });
+  }
+};
 
-var getWeather = function(data) {
-    var {name} = data;
-    var {icon,description}= data.weather[0];
-    var {temp,humidity}= data.main;
-    console.log (name,icon,temp,humidity,description);
-    nameEl.innerText = "Temp: "+ temp + "F";
-    iconEl.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-    humidityEl.innerText = "Humidity: "+ humidity;
-    descEl.innerText = description;
-
-}
+//------------------------------------- call functions directly ---------------------------------------//
+loadSavedCity();
+//-------------------------- create button with searched city ends here -------------------------------//
+//--------------------------- event handler from submit form starts here ------------------------------//
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  // name of the city
+  var searchCityName = $("#searchCity").val().trim();
+  var newcity = saveCityName(searchCityName);
+  getCityWeather(searchCityName);
+  if (newcity == 1) {
+    createCityNameBtn(searchCityName);
+  }
+};
+var BtnClickHandler = function (event) {
+  event.preventDefault();
+  // name of the city
+  var searchCityName = event.target.textContent.trim();
+  getCityWeather(searchCityName);
+};
+//--------------------------- event handler from submit form ends here ------------------------------//
+//------------------------ call functions with submit button starts here ----------------------------//
+$("#searchCityForm").on("submit", function () {
+  formSubmitHandler(event);
+});
+$(":button.list-group-item-action").on("click", function () {
+  BtnClickHandler(event);
+});
+//-------------------------- call functions with submit button ends here ----------------------------//
